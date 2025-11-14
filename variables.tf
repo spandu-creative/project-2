e103a5f52adff8d464fbf7e2fc0e492347c69222
spandu-creative <spandanas320@gmail.com>
2025-11-14 14:30:20 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "gcp_project" {}
+variable "gcp_region" {
+  default = "asia-south1"
+}
+
+variable "gcp_zone" {
+  default = "asia-south1-a"
+}
